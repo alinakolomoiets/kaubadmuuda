2b524770d7eb1d9473a4935cbc38171e0c784c09
Alina <93324365+alinakolomoiets@users.noreply.github.com>
2022-11-18 10:55:58 +0200

    Create style_kauba.php

---
+++ style_kauba.php
@@ -0,0 +1,31 @@
+#menu{
+    padding: 30px;
+    float: left;
+    background-color: darkseagreen;
+    left:100px;
+    position: absolute;
+
+}
+#menu1{
+    padding: 30px;
+    float: left;
+    background-color: darkseagreen;
+    top:300px;
+    position: absolute;
+
+}
+#menu2{
+    padding: 30px;
+    float: left;
+    background-color: darkseagreen;
+    top:300px;
+    left:350px;
+    position: absolute;
+}
+table#table{
+    border:2pt solid black;
+
+}
+#table td{
+    border:2pt solid black;
+}
